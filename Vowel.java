911f0a0ec75810d13c04610c79b1423aa10a831e
dhivyabhaarathiv <dhivyabhaarathiv@gmail.com>
2017-06-14 14:35:20 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Vowel
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char c=sc.next().charAt(0);
+        if(c=='a' || c=='A' || c=='e' || c=='E' || c=='i'|| c=='I' ||c=='o' ||c=='O'
+        || c=='u' || c=='U')
+        {
+            System.out.println("it is an alphabet");
+        }
+        else
+        {
+            System.out.println("it is a consonant");
+        }
+}
+}
